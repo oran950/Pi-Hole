ec1c67ba667c71b44f3b5ae2a724eecc29d99f9d
Oran Ben David <43114098+oran950@users.noreply.github.com>
2022-04-30 19:26:11 +0300

    Create payngo.py

---
+++ payngo.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text=requests.get('https://www.payngo.co.il/computers-pcs/computing-gaming/laptops.html').text
+soup = BeautifulSoup(html_text, 'lxml')
+products = soup.find_all('li',class_='item product product-item product-item-5')
+
+for product in products:
+    price = product.find('span',class_='price').text.replace(' ', '')
+    computer_type = product.find('strong', class_='product name product-item-name product_name').text.replace(' ','')
+    more_info = product.div.a['href']
+    print(f"Type Name:{computer_type.strip()}")
+    print(f"Price:{price}")
+    print(f"More Info:{more_info}")
+    print(" ")
+
+
+
