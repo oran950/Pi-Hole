f8215dc2bcb71e0415b6b55ed17e9e1c319b0ab1
Oran Ben David <43114098+oran950@users.noreply.github.com>
2022-04-30 19:44:34 +0300

    Update payngo.py

--- payngo.py
+++ payngo.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
-import requests
+import requests # call to the site 
 
-html_text=requests.get('https://www.payngo.co.il/computers-pcs/computing-gaming/laptops.html').text
-soup = BeautifulSoup(html_text, 'lxml')
-products = soup.find_all('li',class_='item product product-item product-item-5')
+html_text=requests.get('https://www.payngo.co.il/computers-pcs/computing-gaming/laptops.html').text # call to the site 
+soup = BeautifulSoup(html_text, 'lxml')# scan the html text
+products = soup.find_all('li',class_='item product product-item product-item-5')#'li' is the attributes that contain the products on site.
 
 for product in products:
-    price = product.find('span',class_='price').text.replace(' ', '')
+    price = product.find('span',class_='price').text.replace(' ', '')# span - is attributes that contain the price
     computer_type = product.find('strong', class_='product name product-item-name product_name').text.replace(' ','')
     more_info = product.div.a['href']
     print(f"Type Name:{computer_type.strip()}")
